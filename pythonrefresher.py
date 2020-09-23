7e64a6b1936731af318f15b5964b7131ce6bed5c
Thomaz Avila Barros <thomazavila@gmail.com>
2020-09-23 00:22:09 -0300

    Udemy Django 3: 2 initials Python refreshers.

---
+++ pythonrefresher.py
@@ -0,0 +1,10 @@
+age = 31
+
+name = "Thomaz"
+
+print(f'Hello my name is {name} and I am {age} years old.')
+
+if  age >= 18:
+    print ("You're older than 18.")
+else:
+    print ("You're younger than 18.")
---
+++ pythonrefresher2.py
@@ -0,0 +1,6 @@
+year = 2115
+
+if year >= 2001 and year < 2101:
+    print("Welcome to the 21st century!")
+else:
+    print("You are before or after the 21st century.")
