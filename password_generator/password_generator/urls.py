654263f056bd1ba935b8ef2cf676824a1995fdc4
Thomaz Avila Barros <thomazavila@gmail.com>
2020-10-17 20:05:47 -0300

    Links criados.

--- password_generator/password_generator/urls.py
+++ password_generator/password_generator/urls.py
@@ -18,7 +18,8 @@ from django.urls import path
 from generator import views
 
 urlpatterns = [
-    path('', views.home),
-    path('generatedpassword/', views.password, name='password'),
-    path('admin/', admin.site.urls),
+    path('', views.home, name = 'home'),
+    path('generatedpassword/', views.password, name = 'password'),
+    path('admin/', admin.site.urls, ),
+    path('about/', views.about, name = 'about'),
 ]
