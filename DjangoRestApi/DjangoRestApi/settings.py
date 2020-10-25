c2e7a4749b8dd27859503f6b0223d51fd03548a4
Thomaz Avila Barros <thomazavila@gmail.com>
2020-10-25 13:13:55 -0300

    Uploading what was missing.

--- DjangoRestApi/DjangoRestApi/settings.py
+++ DjangoRestApi/DjangoRestApi/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     # CORS
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
