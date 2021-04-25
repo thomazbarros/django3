1f7396a3bc3c48f085d42dd548565ea1821decc5
Thomaz Avila Barros <thomazavila@gmail.com>
2021-04-25 03:05:35 -0300

    Remembering some fundamentals from Password Generator.

--- personal_portfolio/portfolio/models.py
+++ personal_portfolio/portfolio/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Project (models.Model):
     title = models.CharField(max_length = 100)
     description = models.CharField(max_length = 250)
-    image = models.ImageField(upload_to='portfolio/images/')
+    image = models.FileField(upload_to='recordings/files/')
     url = models.URLField(blank=True)
     
--- personal_portfolio/portfolio/views.py
+++ personal_portfolio/portfolio/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from portfolio.models import Project
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'portfolio/home.html')
+    projects = Project.objects.all()
+    return render(request, 'portfolio/home.html', {'projects': projects})
