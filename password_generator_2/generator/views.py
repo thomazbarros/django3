097782ada89aac90ad329b47017e113ce58ceb31
Thomaz Avila Barros <thomazavila@gmail.com>
2021-05-15 23:59:45 -0300

    Using render to return an html page.

--- password_generator_2/generator/views.py
+++ password_generator_2/generator/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Hello there friend!')
+    return render(request, 'generator/home.html')
 
 def eggs(request):
     return HttpResponse('<h1>Eggs are so tasty!</h1>')
