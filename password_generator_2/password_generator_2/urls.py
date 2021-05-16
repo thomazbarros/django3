b9ac4c2fc35e62947ac6179abc9f99fb7b7c99a6
Thomaz Avila Barros <thomazavila@gmail.com>
2021-05-16 19:17:06 -0300

    Action on a form.

--- password_generator_2/password_generator_2/urls.py
+++ password_generator_2/password_generator_2/urls.py
@@ -19,6 +19,6 @@ from generator import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('eggs/', views.eggs),
+    path('generatedpassword/', views.password, name = 'password'),
     path('', views.home),
 ]
