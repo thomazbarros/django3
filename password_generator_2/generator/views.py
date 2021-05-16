5f53775d51f71198f6f29c3dcf0867ab93b9f0b6
Thomaz Avila Barros <thomazavila@gmail.com>
2021-05-16 16:33:03 -0300

    Passing a parameter for the first time.

--- password_generator_2/generator/views.py
+++ password_generator_2/generator/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'generator/home.html')
+    return render(request, 'generator/home.html', {'password': 'will be random.'})
 
 def eggs(request):
     return HttpResponse('<h1>Eggs are so tasty!</h1>')
